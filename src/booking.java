72c01968a53da2eadcb123faf360646d908060a0
Sander-sach <sach0004@stud.ek.dk>
2025-10-27 17:03:07 +0100

    Booking defineret samt metode til addbooking. metoden:bruger tids variabler: LocalDateTime den formatter en String tastet i konsolen til en tids variabel så man kan skrive navn og booking tid ind.

--- src/booking.java
+++ src/booking.java
@@ -4,44 +4,52 @@ import java.time.format.DateTimeParseException;
 import java.util.*;
 
 public class booking {
-    // tilføj tlf nummer som variabel dette skal være søge variablen
+    String telefon;
     String navn;
     String tid;
     LocalDateTime time;
-    Scanner input=new Scanner(System.in);
+    Scanner input=new Scanner(System.in); // tager kunde info + booking tid fra tastatur
+
+    // DateTimeformatter sætter et format for hvordan dato skal skrives
+    //indtastning i terminalen skal altså skrives i dette format for at virke.
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
-    booking(String navn, LocalDateTime time){
+    // Constructer starter en Booking med navn, tlf, tid.
+    booking(String navn,String telefon, LocalDateTime time){
         this.navn=navn;
         this.time=time;
-
-
+        this.telefon=telefon;
     }
 
+
+
+
     public void addBooking(){
+        // Indtast kunde navn og telefoner nummer i booking
         navn = input.nextLine();
+        telefon = input.nextLine();
+        // While loop spørg efter en gyldig dato
+        //indskrevet dato som String skal matche formattet dd-MM-yyyy HH:mm ellers fejler den
         while(true) {
             this.tid = input.nextLine();
             try {
                 LocalDateTime tid = LocalDateTime.parse(this.tid, formatter);
                 time=tid;
                 break;
+                //catch fanger fejl som skulle opstå hvis der indtastet forkert dato format
             } catch (DateTimeParseException k) {
                 System.out.println("Ikke gyldig dato eller tid");
             }
         }
         input.close();
     }
+    // ToString lavet til test
     public String toString(){
-        return navn+"\t"+time;
+        return navn+"\t"+telefon+"\t"+time;
     }
 
-
-
-
-
     public static void main(String[]arg){
-        booking a1=new booking(null,null);
+        booking a1=new booking(null,null,null);
         a1.addBooking();
         System.out.println(a1);
 
