266c7007af4195b65a19470f8a2866cbd5751245
Sander-sach <sach0004@stud.ek.dk>
2025-10-27 13:31:42 +0100

    Booking defineret samt metode til addbooking. metoden:bruger tids variabler: LocalDateTime den formatter en String tastet i konsolen til en tids variabel så man kan skrive navn og booking tid ind.

---
+++ src/booking.java
@@ -0,0 +1,49 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.*;
+
+public class booking {
+    String navn;
+    String tid;
+    LocalDateTime time;
+    Scanner input=new Scanner(System.in);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    booking(String navn, LocalDateTime time){
+        this.navn=navn;
+        this.time=time;
+
+
+    }
+
+    public void addBooking(){
+        navn = input.nextLine();
+        while(true) {
+            this.tid = input.nextLine();
+            try {
+                LocalDateTime tid = LocalDateTime.parse(this.tid, formatter);
+                time=tid;
+                break;
+            } catch (DateTimeParseException k) {
+                System.out.println("Ikke gyldig dato eller tid");
+            }
+        }
+        input.close();
+    }
+    public String toString(){
+        return navn+"\t"+time;
+    }
+
+
+
+
+
+    public static void main(String[]arg){
+        booking a1=new booking(null,null);
+        a1.addBooking();
+        System.out.println(a1);
+
+
+    }//Main
+}
